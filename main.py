9be651f47335c1957afb084e6fae033dc4536fb9
szawel <na.adress@gmail.com>
2022-08-18 21:18:04 +0200

    import JSON settings

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
 from lib import *
-
-orders_list = ['#add', '#del'] #list of list operation functions
-utils_list = ['#end', '#show'] #list of utility functions
-
-# a_list = ['kon']
-
-# while True:
-#     user_function = input('Wybierz fuinkcje: ')
-#     if user_function in f_list:
-#         user_input = input('podaj dane: ')
-#         user_function = locals()[user_function]
-#         user_function(user_input)
-#     elif user_function in u_list:
-#         user_function = locals()[user_function]
-#         user_function()
-#     else:
-#         print('nie ma takiej funkcji ani operacji' , end='\n')
-
-# from lib.core import basic
-
-# while True:
-#     user_input = input()
-#     todo = basic(user_input)
-#     print(todo[0], todo[1])
-
+import json
 
 def main():
 
+    with open('settings\orders.json') as f:
+        data = json.load(f)
+
+    # load orders list
+    for state in data['user_orders']:
+        orders_list = state['list_orders']
+
+    # load utility list 
+    for state in data['user_orders']:
+        utility_list = state['utility_orders']
+
+    user_order_lists = orders_list + utility_list
+
+    for i in data['user_orders']:
+        print(i['list_orders'])
 
     while True:
         print("----- ----- ----- ----- ----- ")
         user_input = input('Wybierz fuinkcje: ')
-        user_input = BasicInterpreter(user_input, orders_list)
+        user_input = BasicInterpreter(user_input, user_order_lists)
         user_input = user_input.split()
 
         if user_input[0] in orders_list:
             print("new order: ", user_input)
             # user_input = locals()[user_input]
             # user_input(user_input)
-        elif user_input[0] in utils_list:
-            print("Utilty order: ")
+        elif user_input[0] in utility_list:
+            print("Utilty order: ", user_input)
             # user_input = locals()[user_input]
             # user_input()
         else:
-            print('nothing that can be done was given' , end='\n')
+            print('nothing can be done with given data' , end='\n')
 
 
 if __name__ == '__main__':
