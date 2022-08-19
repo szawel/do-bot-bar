b2fbd8c92412c80a0a1e4d1ddb667c49319f6224
szawel <na.adress@gmail.com>
2022-08-19 21:01:54 +0200

    small cleanup

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,5 @@
 from .basic_interpreter import BasicInterpreter
+from .basic_divider import BasicDivider
 from .order import order_add
 from .order import order_del
 from .order import order_show
--- main.py
+++ main.py
@@ -4,45 +4,59 @@ import json
 
 def main():
 
-    # load order file
+    # load order file nad all lists
     with open('settings\orders.json') as f:
         data = json.load(f)
 
-    # 1. load orders list
     for state in data['user_orders']:
         orders_list = state['list_orders']
 
-    # 2. load utility orders list 
     for state in data['user_orders']:
         utility_list = state['utility_orders']
 
     # merge list of posibel orders
     user_order_lists = orders_list + utility_list
 
-    order_add("some data")
-
     # 3. Run main while
     while True:
         print("                              ")
         user_input = input('Type new order: ')
+        print('user_input:       ', user_input)
+        
         user_input = BasicInterpreter(user_input, user_order_lists)
-        user_input = user_input.split()
+        print('BasicInterpreter: ', user_input)
 
-        if len(user_input) != 0:
-            if user_input[0] in orders_list:
-                print("new order: ", user_input, end='\n')
-                # user_input = locals()[user_input]
-                # user_input(user_input)
-            elif user_input[0] in utility_list:
-                print("Utilty order: ", user_input, end='\n')
-                # user_input = locals()[user_input]
-                # user_input()
+        ja = BasicDivider(user_input, user_order_lists)
+        print('BasicDivider:     ', ja)
+
+        # user_input = user_input.split()
+
+        for x in ja:
+            user_data = x
+            print('user_data:        ', user_data)
+            user_data = user_data.split()
+            print('user_data:        ', user_data)
+
+            if user_data[0] in orders_list:
+                print("Order is found [OK]")
             else:
-                print('nothing can be done with given data' , end='\n')
-                print("                              ")
-        else:
-            print('! no data or no useful data was given !' , end='\n')
-            pass
+                print("! no Order is found !")
+
+        # if len(user_input) != 0:
+        #     if user_input[0] in orders_list:
+        #         print("new order: ", user_input, end='\n')
+        #         # user_input = locals()[user_input]
+        #         # user_input(user_input)
+        #     elif user_input[0] in utility_list:
+        #         print("Utilty order: ", user_input, end='\n')
+        #         # user_input = locals()[user_input]
+        #         # user_input()
+        #     else:
+        #         print('nothing can be done with given data' , end='\n')
+        #         print("                              ")
+        # else:
+        #     print('! no data or no useful data was given !' , end='\n')
+        #     pass
 
 
 if __name__ == '__main__':
